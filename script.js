f5fe508c394a2d7e4f3b8ac92b6a03f75de7a8c8
ilhamAndi576 <ilhamandi1790@gmail.com>
2024-10-31 17:38:19 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+// Untuk sementara belum ada JavaScript tambahan. 
+// JavaScript dapat digunakan untuk interaksi lebih lanjut, seperti galeri foto interaktif atau formulir kontak dinamis.
+console.log("Website Wisata Religi siap!");
